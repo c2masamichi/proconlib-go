78a4067acb832a0739fc93a9627eec3be7b04fa5
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-08-27 22:28:12 +0900

    min for int

--- math/prog_test.go
+++ math/prog_test.go
@@ -23,3 +23,23 @@ func TestMax(t *testing.T) {
 		}
 	}
 }
+
+func TestMin(t *testing.T) {
+	parameters := []struct {
+		a        int
+		b        int
+		expected int
+	}{
+		{2, 1, 1},
+		{3, 4, 3},
+		{5, 5, 5},
+	}
+
+	for i, param := range parameters {
+		a, b, expected := param.a, param.b, param.expected
+		actual := min(a, b)
+		if actual != expected {
+			t.Errorf("i: %d\nactual: %v\nexpected: %v", i, actual, expected)
+		}
+	}
+}
